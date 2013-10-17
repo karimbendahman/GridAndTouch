7cddb085495010a8852ab65c153150c534bff760
karimbendahman <hhs@hhss-mac-mini.wireless.hhs.nl>
2013-10-17 16:21:38 +0200

    the Grid and touch control Jordi en Robin
    made the grid for the photo and touch for i spy
    
    Jordi en Robin

--- GridAndTouch/ViewController.h
+++ GridAndTouch/ViewController.h
@@ -2,12 +2,18 @@
 //  ViewController.h
 //  GridAndTouch
 //
-//  Created by Robin on 10/16/13.
-//  Copyright (c) 2013 Robin. All rights reserved.
+//  Created by Jordi en Robin on 10/16/13.
+//  Copyright (c) 2013 Jordi en Robin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (strong, nonatomic) NSMutableArray *gridX;
+@property (nonatomic) BOOL touchMoved;
+@property (nonatomic) int margin;
+- (CGFloat) screenSizeX;
+- (CGFloat) screenSizeY;
 @end
+
+
